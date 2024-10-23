b5790a58bacda10379c2b2c05df703a399de8867
Quentin Huan <quentinhuan@laposte.net>
2024-10-23 23:11:10 +0200

    Initial commit

---
+++ install_config.sh
@@ -0,0 +1,31 @@
+## git
+apt install git
+
+# keepassxc
+apt install keepassxc
+
+## i3 window manager
+apt install i3-wm
+apt install suckless-tools
+
+## tmux
+apt install tmux
+
+## zsh
+apt install zsh
+
+## neovim
+apt install neovim
+
+## various tools
+apt install htop
+apt install curl
+apt install mesa-utils
+
+## bib
+
+## bib
+sudo apt-get install libc6-i386
+
+## rhythmbox
+apt install rhythmbox
