63a5dbc9efc9e70a77ce6a42ec7dc5d9da4509a7
QuentinHuan <quentinhuan@laposte.net>
2025-08-26 23:46:17 +0200

    Automatic Vim-Plug installation

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
+-- Autoinstall Vim-plug
+vim.cmd([[
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+    silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+    endif
+    ]])
+
 local vim = vim
 local Plug = vim.fn['plug#']
 
