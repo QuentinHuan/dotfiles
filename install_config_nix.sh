617efe574b9ced26733fd3ca5513bdd61bc66352
QuentinHuan <quentinhuan@laposte.net>
2025-10-03 13:18:48 +0200

    update install script

--- install_config_nix.sh
+++ install_config_nix.sh
@@ -1,3 +1,16 @@
+echo "This program may requiere sudo priviledge"
+
+echo "Copy configuration.nix into /etc/nixos/"
+cp configuration.nix /etc/nixos/
+
+echo "Copy .config into home directory"
+cp -a .config /home/qhuan/
+
 # install vimplug
+echo "Download Vimplug "
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Finish
+echo "DONE"
+echo "Consider running : nixos-rebuild boot    to finish the install"
